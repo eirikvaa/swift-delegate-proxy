516d365b8c551f5fd7893f45013c5baf696ff60d
Eirik Vale Aase <e.v.aase@gmail.com>
2022-08-20 09:21:20 +0200

    Add initial library implementation

--- Sources/SwiftDelegateProxy/SwiftDelegateProxy.swift
+++ Sources/SwiftDelegateProxy/SwiftDelegateProxy.swift
@@ -1,6 +1,45 @@
-public struct swift_delegate_proxy {
-    public private(set) var text = "Hello, World!"
+/// A request is sent to a proxy that corresponds to an event of the particular delegate you wish
+/// to communicate with. For instance, requesting a single location from CoreLocation would require
+/// a SingleLocationRequest and a SingleLocationEvent.
+public protocol Request {
+    associatedtype Value
+    associatedtype RequestError: Error
 
-    public init() {
+    var events: [Event.Type] { get }
+    func linkContinuation(_ continuation: CheckedContinuation<Value, RequestError>)
+    func response(_ event: any Event)
+}
+
+public extension Request {
+    func respondsToEvent(_ eventType: Event.Type) -> Bool {
+        events.contains(where: { $0 == eventType })
+    }
+}
+
+/// An event that corresponds to a delegate method you wish to obtain results from.
+public protocol Event {}
+
+/// The proxy mediates between your async data provider and the delegate you wish to
+/// wrap async access over.
+public class Proxy {
+    public var requests: [any Request]
+
+    public init(requests: [any Request] = []) {
+        self.requests = requests
+    }
+
+    public func addRequest(_ request: any Request) {
+        requests.append(request)
+    }
+
+    public func respond(_ event: some Event) {
+        guard let requestIndex = requests.firstIndex(where: { $0.respondsToEvent(type(of: event)) }) else {
+            return
+        }
+
+        let request = requests[requestIndex]
+
+        request.response(event)
+        requests.remove(at: requestIndex)
     }
 }
